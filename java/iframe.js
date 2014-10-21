ef1ce3be509f0ecc6a7d0c38ac30796ca717ea51
Jackson Williams <jcw1395@gmail.com>
2014-10-21 12:29:41 -0400

    Update iframe.js

--- java/iframe.js
+++ java/iframe.js
@@ -8,8 +8,8 @@ function pageY(elem) {
 function resizeIframe() {
     var height = document.documentElement.clientHeight;
     height -= pageY(document.getElementById('https://docs.google.com/document/d/1Zm5k7HUNcLVbAnniQxctmQJO2Km6qmf-FAsq_1D5Vs8/pub?embedded=true'))+ buffer ;
-    height = (height < 100) ? 100 : height;
-    document.getElementById('https://docs.google.com/document/d/1Zm5k7HUNcLVbAnniQxctmQJO2Km6qmf-FAsq_1D5Vs8/pub?embedded=true').style.height = '100px';
+    height = (height < 0) ? 0 : height;
+-    document.getElementById('https://docs.google.com/document/d/1Zm5k7HUNcLVbAnniQxctmQJO2Km6qmf-FAsq_1D5Vs8/pub?embedded=true').style.height = height + 'px';
 }
 
 // .onload doesn't work with IE8 and older.
