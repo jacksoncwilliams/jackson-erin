b0511ed2e23dd59354fcb0a831f135a5376a97ec
Jackson Williams <jcw1395@gmail.com>
2015-08-03 23:20:17 -0400

    Update simple.js

--- java/simple.js
+++ java/simple.js
@@ -1,9 +1,8 @@
 // JavaScript Document
 
-$(".more").click(function(e){
-  var _this = $($(this).attr("href"));
-  $("body, html").animate({
-    scrollTop: _this.offset().top -72
-  }, 1000)
-  });
-
+$("more").click(function(e){
+	var _this = $($(this).attr("href"));
+	$("body, html").animate({
+		scrollTop: _this.offset().top -72
+	}, 1000)
+});
