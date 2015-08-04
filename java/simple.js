85f1625ad9b03ee094db7869e25dbfe7bd026f72
Jackson Williams <jcw1395@gmail.com>
2015-08-03 23:13:27 -0400

    Update simple.js

--- java/simple.js
+++ java/simple.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 $(".more").click(function(e){
-  var_this = $($(this).attr("href"));
+  var _this = $($(this).attr("href"));
   $("body, html").animate({
     scrollTop: _this.offset().top -72
   }, 1000)
