2a598cc655c36d6bab9f8ebd48139e29b8c64ba5
Jackson Williams <jcw1395@gmail.com>
2018-06-02 22:32:44 -0400

    Update jquerymenu.js

--- java/jquerymenu.js
+++ java/jquerymenu.js
@@ -3,12 +3,5 @@ jQuery('.toggle-nav').click(function(e) {
 jQuery(this).toggleClass('active');
 jQuery('.menu ul').toggleClass('active');
 e.preventDefault();  
-              var $root = $('html, body');
-$('a').click(function() {
-    $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-});
 });
 });
